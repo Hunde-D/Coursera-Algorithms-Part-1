fbb70a26440be9522ae5d62c27e8fb24aae7b5ef
Hunde-D <hunddh2025@gmail.com>
2023-11-06 19:06:48 +0300

    added PercolationStats.java

--- Practice_Algorithms/QuickFindUF.java
+++ Practice_Algorithms/QuickFindUF.java
@@ -6,24 +6,27 @@ import edu.princeton.cs.algs4.StdOut;
 public class QuickFindUF {
 
     private int[] id;
-    public QuickFindUF(int N){
+
+    //set id of each object to itself
+    public QuickFindUF(int N) {
         id = new int[N];
         for (int i = 0; i < N; i++)
             id[i] = i;
     }
 
+    //check whether p and q are in the same component
     public boolean connected(int p, int q) {
         return id[p] == id[q];
     }
 
+    //change all entries with id[p] to id[q]
     public void union(int p, int q) {
         int pid = id[p];
         int qid = id[q];
-        for(int i = 0; i < id.length; i++) {
-            if(id[i] == pid)
-                id[i] = qid;
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pid) id[i] = qid;
         }
-    }
+
     public static void main(String[] args) {
         int size = StdIn.readInt();
         QuickFindUF quickFindUF = new QuickFindUF(size);
